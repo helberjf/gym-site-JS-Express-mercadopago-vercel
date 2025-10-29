cab1230bf01d447a5e90722232a51ec93b9c6d2a
Helber Soares <139668970+helberjf@users.noreply.github.com>
2025-10-29 05:03:48 -0300

    Add Mercado Pago SDK initialization
    Initialize Mercado Pago SDK with access token.

---
+++ main.js
@@ -0,0 +1,4 @@
+// SDK do Mercado Pago
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+// Adicione credenciais
+const client = new MercadoPagoConfig({ accessToken: 'APP_USR-635176720903899-102822-9acbde29f169292a83f82b80f4602b6e-2954028260' });
